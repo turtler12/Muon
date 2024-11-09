d98b49ad6ea33f3c739fc696aac7c08d393d17fa
KellerJordan <kellerjordan1998@gmail.com>
2024-11-09 05:00:54 +0000

    .

--- muon.py
+++ muon.py
@@ -138,7 +138,7 @@ class Muon(torch.optim.Optimizer):
 
                 bias_correction1 = 1 - beta1**step
                 bias_correction2 = 1 - beta2**step
-                scale = bias_correction1 / bias_correction2.sqrt()
+                scale = bias_correction1 / bias_correction2**0.5
                 param.mul_(1 - lr * weight_decay)
                 p.data.add_(g, alpha=-lr/scale)
 
